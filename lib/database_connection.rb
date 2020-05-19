e1d1610c590122609f06c884748385a05a3b7dd0
Asia <asiaellis5@gmail.com>
2020-05-19 15:52:07 +0100

    added method to check connection is persistent

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,6 +3,10 @@ require 'pg'
 class DatabaseConnection
 
   def self.setup(dbname)
-    PG.connect(dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.connection 
+    @connection
   end
 end
--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -2,7 +2,12 @@ require 'database_connection'
 
 describe ".setup" do
   it "sets up a connection to a database through PG" do
-    expect(PG).to receive(:connect).with('recipe_manager_test')
+    expect(PG).to receive(:connect).with(dbname: 'recipe_manager_test')
     DatabaseConnection.setup('recipe_manager_test')
+  end
+
+  it "keeps the connection persistent" do
+    connection = DatabaseConnection.setup('recipe_manager_test')
+    expect(DatabaseConnection.connection).to eq connection 
   end
 end
