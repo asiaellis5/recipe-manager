287c614b6a44396bb6a3d7b94d5c5334f897539c
Asia <asiaellis5@gmail.com>
2020-05-19 15:59:38 +0100

    replaced PG in recipes with database connection wrapper

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'pg'
+require 'database_connection'
 
 class Recipe
   attr_reader :title, :url, :id
@@ -12,29 +12,25 @@ class Recipe
   end
 
   def self.all
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'recipe_manager_test') : connection = PG.connect(dbname: 'recipe_manager')
-    result = connection.exec('SELECT * FROM recipes')
+    result = DatabaseConnection.query('SELECT * FROM recipes')
     result.map { |recipe| new(recipe['title'], recipe['url'], recipe['id']) }
   end
 
   def self.create(url, title)
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'recipe_manager_test') : connection = PG.connect(dbname: 'recipe_manager')
-    result = connection.exec("INSERT INTO recipes (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title;")
+    result = DatabaseConnection.query("INSERT INTO recipes (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title;")
   end
 
   def self.update(id, url, title)
     ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'recipe_manager_test') : connection = PG.connect(dbname: 'recipe_manager')
-    result = connection.exec("UPDATE recipes SET url= '#{url}', title= '#{title}' WHERE id= '#{id}'")
+    result = DatabaseConnection.query("UPDATE recipes SET url= '#{url}', title= '#{title}' WHERE id= '#{id}'")
   end
 
   def self.delete(id)
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'recipe_manager_test') : connection = PG.connect(dbname: 'recipe_manager')
-    result = connection.exec("DELETE FROM recipes WHERE id = '#{id}'")
+    result = DatabaseConnection.query("DELETE FROM recipes WHERE id = '#{id}'")
   end
 
   def self.find(id)
-    ENV['ENVIRONMENT'] == 'test' ? connection = PG.connect(dbname: 'recipe_manager_test') : connection = PG.connect(dbname: 'recipe_manager')
-    result = connection.exec("SELECT * FROM recipes WHERE id = #{id};")
+    result = DatabaseConnection.query("SELECT * FROM recipes WHERE id = #{id};")
     self.new(result[0]['title'], result[0]['url'], result[0]['id'])
   end
 end
