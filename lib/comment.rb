ff44d0d50289af7325c68e0f3f525ee748f05d34
Asia <asiaellis5@gmail.com>
2020-05-21 13:12:09 +0100

    added a comment class and added all and create methods

---
+++ lib/comment.rb
@@ -0,0 +1,21 @@
+require_relative 'database_connection'
+
+class Comment
+
+  attr_reader :comment, :id, :recipe_id
+
+  def initialize(id, comment, recipe_id)
+    @id = id
+    @comment = comment
+    @recipe_id = recipe_id
+  end
+  
+  def self.all
+    result = DatabaseConnection.query('SELECT * FROM comments')
+    result.map { |comment| new(comment['id'], comment['comment'], comment['recipe_id']) }
+  end
+
+  def self.create(comment, recipe_id)
+    result = DatabaseConnection.query("INSERT INTO comments (comment, recipe_id) VALUES ('#{comment}', '#{recipe_id}') RETURNING id, comment, recipe_id;")
+  end
+end
