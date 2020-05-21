b847430918490ea4e3e3e4d31327646f4602e5ee
Asia <asiaellis5@gmail.com>
2020-05-21 15:44:54 +0100

    added a find method to user

--- lib/user.rb
+++ lib/user.rb
@@ -15,6 +15,13 @@ class User
 
   def self.create(email, password)
     result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES ('#{email}', '#{password}') RETURNING id, email, password;")
+    result.map { |user| new(user['id'], user['email'], user['password']) }
+  end
+
+  def self.find(id)
+    return nil unless id
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
+    result.map { |user| new(user['id'], user['email'], user['password']) }
   end
   
 end
