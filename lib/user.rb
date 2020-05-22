26c79f144cd1e2e17ff0170cc4a5bce1296d2a7f
Asia <asiaellis5@gmail.com>
2020-05-22 12:52:11 +0100

    fixed bug that wouldnt allow check password

--- lib/user.rb
+++ lib/user.rb
@@ -30,7 +30,7 @@ class User
   def self.authenticate(email, password)
     result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
     return nil unless result.any?
-    return nil unless BCrypt::Password.new(result[0]['password']) == password
+    return nil unless BCrypt::Password.create(result[0]['password']) == password
     User.new(result[0]['id'], result[0]['email'], result[0]['password'])
   end
   
